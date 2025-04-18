17f44adf25478f3a821059706d8bead51a57712d
Ben Diesel <befkotze@gmail.com>
2025-04-18 07:59:08 +0200

    Add Vogen converters to data layer

--- Infrastructure/ApplicationDbContext.cs
+++ Infrastructure/ApplicationDbContext.cs
@@ -1,10 +1,19 @@
 using Microsoft.EntityFrameworkCore;
 using System.Reflection;
+using EfCoreDemo.Infrastructure.Conversion;
 
 namespace EfCoreDemo.Infrastructure;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
 {
+    protected override void ConfigureConventions(
+        ModelConfigurationBuilder configurationBuilder)
+    {
+        base.ConfigureConventions(configurationBuilder);
+
+        configurationBuilder.RegisterAllInVogenEfCoreConverters();
+    }
+    
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         // Scan for all IEntityTypeConfiguration<> in the current assembly
---
+++ Infrastructure/Conversion/VogenEfCoreConverters.cs
@@ -0,0 +1,12 @@
+﻿using Vogen;
+
+namespace EfCoreDemo.Infrastructure.Conversion;
+
+[EfCoreConverter<Domain.AccountId>]
+[EfCoreConverter<Domain.BranchCode>]
+[EfCoreConverter<Domain.CustomerId>]
+[EfCoreConverter<Domain.FirstName>]
+[EfCoreConverter<Domain.LastName>]
+[EfCoreConverter<Domain.TransactionId>]
+[EfCoreConverter<Domain.Money>]
+internal partial class VogenEfCoreConverters;
